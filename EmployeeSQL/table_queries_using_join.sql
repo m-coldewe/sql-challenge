067255ee1737eb926b41db207f73934a002865da
m-coldewe <152045367+m-coldewe@users.noreply.github.com>
2024-02-24 13:27:37 -0500

    second commit, includes updated ERD, table schemata, and both join versions of the table query sql

--- EmployeeSQL/table_queries_using_join.sql
+++ EmployeeSQL/table_queries_using_join.sql
@@ -1,53 +1,54 @@
 --1. List the employee number, last name, first name, sex, and salary of each employee.
-select 
-	e.emp_no, 
-	e.last_name, 
-	e.first_name, 
-	e.sex, 
-	s.salary
-from employees as e
-inner join salaries as s on
-e.emp_no=s.emp_no;
+select
+	emp_no,
+	last_name,
+	first_name,
+	sex,
+	salary
+from employees
+join salaries
+using (emp_no);
 
 
 --2. List the first name, last name, and hire date for the employees who were hired in 1986.
-
-select 
-	first_name, 
-	last_name, 
+select
+	first_name,
+	last_name,
+	hire_date,
 	date_part('year', hire_date) as year
 from employees
-where date_part('year', hire_date) = 1986;
+where date_part('year', hire_date) = '1986';
 
 
 --3. List the manager of each department along with their department number, department name, employee number,
 --last name, and first name.
---dept_manager, 
+--employees, deptartments, dept_manager
 select
-	dm.dept_no, 
+	d.dept_no,
 	d.dept_name,
-	e.emp_no, 
+	e.emp_no,
 	e.last_name,
 	e.first_name
-from dept_manager as dm
-join employees as e
-using (emp_no)
+from employees as e
+join dept_manager as dm
+on e.emp_no=dm.emp_no
 join departments as d
-using (dept_no);
+on dm.dept_no=d.dept_no;
+
 
 --4. List the department number for each employee along with that employee's employee number, last name, 
 --first name, and department name.
 select 
-	dept_no,
-	emp_no,
-	last_name,
-	first_name,
-	dept_name
-from dept_emp
-join employees
-using (emp_no)
-join departments
-using (dept_no);
+	d.dept_no,
+	e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+from employees as e
+join dept_emp as de
+on e.emp_no=de.emp_no
+join departments as d
+on d.dept_no=de.dept_no;
 
 --5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name
 --begins with the letter B.
@@ -61,37 +62,36 @@ and last_name like 'B%';
 
 --6. List each employee in the Sales department, including their employee number, last name, and first name.
 select
-	dept_name,
-	emp_no,
-	last_name,
-	first_name
-from employees
-join dept_emp
-using (emp_no)
-join departments
-using(dept_no)
+	d.dept_name,
+	e.emp_no,
+	e.last_name,
+	e.first_name
+from employees as e
+join dept_emp as de
+on e.emp_no=de.emp_no
+join departments as d
+on de.dept_no=d.dept_no
 where dept_name = 'Sales';
 
 --7. List each employee in the Sales and Development departments, including their employee number, last name,
 --first name, and department name.
 select
-	emp_no,
-	last_name,
-	first_name,
-	dept_name
-from employees
-join dept_emp
-using (emp_no)
-join departments
-using (dept_no)
+	e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+from employees as e
+join dept_emp as de
+on e.emp_no=de.emp_no
+join departments as d
+on d.dept_no=de.dept_no
 where dept_name = 'Sales' or dept_name = 'Development';
 
 --8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees
 --share each last name).
-select 
-	last_name, 
+select
+	last_name,
 	count(last_name)
 from employees
 group by last_name
 order by count(last_name) desc;
-
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # sql-challenge
 A research project about people whom Pewlett Hackard (a fictional company) employed during the 1980s and 1990s. 
+
+# Project Title
+
+## Overview
+
+A research project looking at the people Pewlett Hackard (a fictional company) employed during the 1980s and 1990s, using information stored in CSV files that was imported to an SQL database. Utilizes ProsgreSQL.
+
+## Purpose
+
+- examined the data provided in the csv files
+- used quickDB to create a table schema for each of the csv files with primary keys, foreign keys, and constraints specified
+- imported files to the corresponding SQL table
+- queried the imported data to generate the following:
+	1. List the employee number, last name, first name, sex, and salary of each employee.
+	2. List the first name, last name and hire date for the employees who were hired in 1986.
+	3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
+	4. List the department number for each employee along with that employee's employee number, last name, first name, and department name.
+	5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+	6. List each employee in the Sales department, including their employee number, last name, and first name.
+	7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+	8. List the frequency counts, in descending order, of all the employee last names.
+
+
+I received assistance on this assignment from Tutor Geronimo. He told me a clearer way to write the queries, which I kept for both versions of the query document as leaving them in a single line proved too confusing for me. He also recommended utiliing "using" for the joins, as it was a simpler and clearer method, with the caveat that it would not always work. He explained that while "using" was simpler, "on" was more reliable. To ensure I understood, after our session, I completed the queries again using "on." Both query documents are included. 
+
+
+## Result
+
+Provide examples of the finished analysis such as graph.
+Explain the results of the analysis.
+
+
+## Summary
+Conclusions of analysis and final words.
